e12e9c3eb82c7062a362931560d7eed39fe5883e
maitabe <martushb@gmail.com>
2016-11-12 20:49:00 +0200

    added alert message and new function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// proto functions
+String.prototype.isEmpty = function() {
+  return this.valueOf() === "";
+}
+
 //global variables
 var reservations = {
   'Bob': { claimed: false },
@@ -6,11 +11,27 @@ var reservations = {
 
 //functions
 
+//fade the alert message after 4 minutes
+var clearAlertMsg = function() {
+
+  setTimeout(function() {
+    $('#alert-message').fadeOut('slow', function(){
+        $('#alert-message').empty();
+        //$('#alert-message').css('display', 'block');
+        $('#alert-message').show();
+    });
+    $('input#name-txt').removeClass('success').removeClass('warning').removeClass('error');
+  }, 4000);
+
+}
+
+
 //claimReservation function for a Restaurant
 var claimReservation = function (nameInput) {
+
   var userExist = false;
     // loop through the reservation obj
-   for( var user in reservations){
+  for( var user in reservations){
       var rsvp = reservations[user];
         // check if user exist
       if(nameInput === user){
@@ -21,19 +42,16 @@ var claimReservation = function (nameInput) {
           // alert('welcome to the show');
           $('#alert-message').append('<p>' + nameInput  + ' reservation was found</p>');
           $('input#name-txt').addClass('success');
+          // $('#reservations-list').append('<li> ' + nameInput + ' has reservation </li>');
         }else if(rsvp.claimed){
           // alert('your reservation was claimed already');
           $('#alert-message').append('<p>' + nameInput  + ' reservation was claimed already</p>');
           $('input#name-txt').addClass('warning');
-        }else{
-          // alert('there is no reservation under your name');
-          $('#alert-message').append('<p>There is no reservation under ' + nameInput + ' your name</p>');
-          $('input#name-txt').addClass('error');
+          // $('#reservations-list').append('<li> ' + nameInput + ' has reservation </li>');
         }
-
-        break;
       }
-   }
+  }
+
 
    //loop is over check if the doesn\'st exist
    if(userExist === false) {
@@ -44,65 +62,74 @@ var claimReservation = function (nameInput) {
         reservations[nameInput] = {claimed:true};
         // alert('reservation completed. Enjoy it!!')
         $('#alert-message').append('<p>Reservation for ' + nameInput  + ' completed</p>');
+        $('#reservations-list').append('<li> ' + nameInput + ' has reservation </li>');
+
     }
    }
 
-//fade the alert message after 4 minutes
-setTimeout(function() {
-    $('#alert-message').fadeOut('slow', function(){
-        $('#alert-message').empty();
-    });
-    $('input#name-txt').removeClass('success').removeClass('warning').removeClass('error');
-}, 4000);
 
+
+    clearAlertMsg();
 };
-
 
 
 // document finish load
 $(document).ready(function() {
 
-// init code
+    // init code
 
-// handlers
-$('#grab-val').click(function() {
+  // handlers
+  $('#grab-val').on('click', function() {
 
 
-// grab value from user input
-  var name = $('#name-txt').val();
-  $('#name-txt').val('');
-  //clean spaces at the beginning or end of string
-  name = name.trim();
+    // grab value from user input
+    var name = $('#name-txt').val();
 
-  //first solution for NON case sensitive
-  var nameConvert = '';
-  for (var i = 0; i < name.length; i++) {
-      //convert first letter of string to caps lock
-      if(i === 0) {
-        //first letter
-        nameConvert += name[0].toUpperCase();
-
-      }else{
-        //convert rest of letters to lower case
-        nameConvert += name[i].toLowerCase();
+     //if input is empty
+      if(name.isEmpty()) {
+        $('#alert-message').append('<p> Please enter a name! </p>');
+         $('input#name-txt').addClass('error');
+         clearAlertMsg();
+         return false;
       }
-  }
-  // //assign NON case sensitive again to name
-  name = nameConvert;
+
+    //clean spaces at the beginning or end of string
+    name = name.trim();
+
+    //first solution for NON case sensitive
+    var nameConvert = '';
+    for (var i = 0; i < name.length; i++) {
+        //convert first letter of string to caps lock
+        if(i === 0) {
+          //first letter
+          nameConvert += name[0].toUpperCase();
+
+        }else{
+          //convert rest of letters to lower case
+          nameConvert += name[i].toLowerCase();
+        }
+    }
+    // //assign NON case sensitive again to name
+    name = nameConvert;
 
 
-  //second solution for NON case sensitive
-  // name = name.toLowerCase();
-  // name = name[0].toUpperCase(); + name.slice(1);
-  // console.log(name);
+    //second solution for NON case sensitive
+    // name = name.toLowerCase();
+    // name = name[0].toUpperCase(); + name.slice(1);
+    // console.log(name);
 
-  claimReservation(name);
+    claimReservation(name);
+
+    $('#name-txt').val('');
+
+  });
+
+
 });
 
 
-});
-
-
+//==================
+//to fix - if alert message still on an the input is clicked the alert message needs to disappear
 
 
 
